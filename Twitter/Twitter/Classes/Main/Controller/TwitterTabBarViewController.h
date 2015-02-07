a552ab660394669ee5b4ad8bce061221763ed2b4
iHannibal <fredisstillalmighty@gmail.com>
2015-02-07 23:37:34 +0800

    Sat Feb 7 23:37 2015

---
+++ Twitter/Twitter/Classes/Main/Controller/TwitterTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwitterTabBarViewController.h
+//  Twitter
+//
+//  Created by Frankenstein Yang on 2/7/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitterTabBarViewController : UITableViewController
+
+@end
---
+++ Twitter/Twitter/Classes/Main/Controller/TwitterTabBarViewController.m
@@ -0,0 +1,22 @@
+//
+//  TwitterTabBarViewController.m
+//  Twitter
+//
+//  Created by Frankenstein Yang on 2/7/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "TwitterTabBarViewController.h"
+
+@implementation TwitterTabBarViewController
+
+/**
+ *  当第一次使用这个类的时候调用1次
+ */
++ (void)initialize {
+    
+    
+    
+}
+
+@end
