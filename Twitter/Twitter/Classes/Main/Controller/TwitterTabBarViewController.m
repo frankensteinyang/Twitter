b88504fd8645bb04b538c0070989895e820463d7
iHannibal <fredisstillalmighty@gmail.com>
2015-02-08 23:32:46 +0800

    Sun Feb 8 23:32 2015

--- Twitter/Twitter/Classes/Main/Controller/TwitterTabBarViewController.m
+++ Twitter/Twitter/Classes/Main/Controller/TwitterTabBarViewController.m
@@ -19,11 +19,44 @@
     
 }
 
+- (void)viewDidLoad {
+
+    [super viewDidLoad];
+    
+}
+
 + (void)setupBarButtonItemTheme {
 
     UIBarButtonItem *appearance = [UIBarButtonItem appearance];
     NSMutableDictionary *textAttrs = [NSMutableDictionary dictionary];
 //    textAttrs[UITextAttributeTextColor] = [UIColor orangeColor];
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    static NSString *ID = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
+    }
+    cell.textLabel.text = @"Testing！";
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return 20;
+    
+}
+
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    UIViewController *newVC = [[UIViewController alloc] init];
+    newVC.view.backgroundColor = [UIColor redColor];
+    // 当push这个控制器的时候，就自动隐藏底部的TabBar
+//    newVC.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:newVC animated:YES];
     
 }
 
