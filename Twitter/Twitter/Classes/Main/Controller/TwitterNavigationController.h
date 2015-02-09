bcbc7126cd878ef44962bd70cb75b483a880a643
iHannibal <fredisstillalmighty@gmail.com>
2015-02-09 23:38:43 +0800

    Mon Feb 9 23:38 2015

---
+++ Twitter/Twitter/Classes/Main/Controller/TwitterNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  TwitterNavigationController.h
+//  Twitter
+//
+//  Created by Frankenstein Yang on 2/9/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitterNavigationController : UINavigationController
+
+@end
---
+++ Twitter/Twitter/Classes/Main/Controller/TwitterNavigationController.m
@@ -0,0 +1,35 @@
+//
+//  TwitterNavigationController.m
+//  Twitter
+//
+//  Created by Frankenstein Yang on 2/9/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "TwitterNavigationController.h"
+
+@implementation TwitterNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+    
+}
+
+- (void)viewDidLoad {
+
+    [super viewDidLoad];
+    
+}
+
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
+
+    [super pushViewController:viewController animated:animated];
+    
+}
+
+@end
