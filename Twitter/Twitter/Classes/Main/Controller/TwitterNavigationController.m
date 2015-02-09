7fb760c9cb8590f1d4e50f8488ff64edfb18275e
iHannibal <fredisstillalmighty@gmail.com>
2015-02-09 23:48:24 +0800

    23:48

--- Twitter/Twitter/Classes/Main/Controller/TwitterNavigationController.m
+++ Twitter/Twitter/Classes/Main/Controller/TwitterNavigationController.m
@@ -26,8 +26,14 @@
     
 }
 
+/**
+ *  能拦截所有push进来的子控制器
+ */
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
 
+    if (self.viewControllers.count > 0) { // 如果现在push的不是栈底控制器（最先push进来的那个控制器）
+        viewController.hidesBottomBarWhenPushed = YES;
+    }
     [super pushViewController:viewController animated:animated];
     
 }
